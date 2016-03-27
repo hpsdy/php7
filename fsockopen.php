3245e376485f2f558b9fc71e0e42546346298768
qinhan <1036474541@qq.com>
2016-03-27 14:02:29 +0800

    hello

--- fsockopen.php
+++ fsockopen.php
@@ -11,16 +11,14 @@ function fopen_m($url)
 	if (!$fp) {
 		echo "$errstr ($errno)<br />\n";
 	} else {
-		$out = "GET / HTTP/1.1\r\n";
-		$out .= "Host: $url\r\n";
-		$out .= "Connection: Close\r\n\r\n";
+		$out = "GET / HTTP/1.1\n";
+		$out .= "Host: $url\n";
+		$out .= "Connection: Close\n\n";
 		fwrite($fp, $out);
-		var_dump(filesize($fp));
-		while (!feof($fp)) {
-			$rel = fread($fp,100000);
-			preg_match("/Content-Length:.?(\d+)/", $rel, $matches);
-			var_dump($matches);
-			//file_put_contents('./msg',$rel,FILE_APPEND);
+			$rel = fgetss($fp,1000);
+			//preg_match("/Content-Length:.?(\d+)/", $rel, $matches);
+			//var_dump($matches);
+			file_put_contents('./msg',$rel,FILE_APPEND);
 		}
 		fclose($fp);
 	}
