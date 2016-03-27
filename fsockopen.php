d6321c499e59f6b5898bb217c35ac9a0508abece
qinhan <1036474541@qq.com>
2016-03-27 11:21:13 +0800

    hello

--- fsockopen.php
+++ fsockopen.php
@@ -1,7 +1,21 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: zhanghuiling
+ * User: qinhan
  * Date: 2016/3/27
  * Time: 11:14
  */
+$fp = fsockopen("www.example.com", 80, $errno, $errstr, 30);
+if (!$fp) {
+	echo "$errstr ($errno)<br />\n";
+} else {
+	$out = "GET / HTTP/1.1\r\n";
+	$out .= "Host: www.example.com\r\n";
+	$out .= "Connection: Close\r\n\r\n";
+	fwrite($fp, $out);
+	while (!feof($fp)) {
+		echo fgets($fp, 128);
+	}
+	fclose($fp);
+}
+?>
